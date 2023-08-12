bf9d2f8d1e8a85c10051c19cf43ef111436f38d5
shiwen cao <126846768+Shiwen-Cao@users.noreply.github.com>
2023-08-12 20:26:36 +0930

    Add files via upload

---
+++ Practical/Practical 1/even.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Practical 1
+//
+//  Created by Mac on 11/8/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
